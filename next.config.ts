17a570118c467e82a9594973e4aab84ae5589511
Stalin Chimbo <gstalin110@gmail.com>
2026-07-18 11:21:11 -0500

    Fix: limpiar next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    outputFileTracing: false,
-  },
+  /* opciones de configuración aquí */
 };
 
 export default nextConfig;
