fc41d53d0bfbc86bd82c5dd24877ae1879ec8f07
Stalin Chimbo <gstalin110@gmail.com>
2026-07-18 11:13:51 -0500

    Fix: mover outputFileTracing dentro de experimental

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  outputFileTracing: false,
+  experimental: {
+    outputFileTracing: false,
+  },
 };
 
 export default nextConfig;
